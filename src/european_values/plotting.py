0e73d7fe59db00b9ed8b22fac84a5b5591f7bd81
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-05-20 14:42:16 +0200

    refactor: Move plotting to separate plotting module

---
+++ src/european_values/plotting.py
@@ -0,0 +1,65 @@
+"""Plotting functions."""
+
+import logging
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.experimental import enable_iterative_imputer  # noqa: F401
+from sklearn.impute import IterativeImputer
+from umap import UMAP
+
+logger = logging.getLogger(__name__)
+
+
+def create_scatter(survey_df: pd.DataFrame, slice_query: str | None = None) -> None:
+    """Create a scatter plot of the survey data.
+
+    Args:
+        survey_df:
+            The survey data.
+        slice_query:
+            The query to slice the data, compatible with DataFrame.query(). If None,
+            the data will not be sliced.
+    """
+    logger.info(f"Shape of the data: {survey_df.shape}")
+
+    if slice_query:
+        logger.info(f"Slicing data with query: {slice_query}")
+        survey_df = survey_df.query(slice_query)
+        logger.info(f"Shape of the sliced data: {survey_df.shape}")
+
+    # Create a 2-dimensional embedding of the EVS trend data
+    logger.info("Imputing missing values...")
+    embedding_matrix = IterativeImputer(
+        estimator=RandomForestClassifier(n_estimators=100, n_jobs=-1),
+        skip_complete=True,
+        n_nearest_features=10,
+        initial_strategy="most_frequent",
+        max_iter=20,
+        random_state=4242,
+    ).fit_transform(survey_df.iloc[:, 3:])
+    logger.info(f"Shape of the imputed data: {embedding_matrix.shape}")
+
+    logger.info("Reducing to two dimensions with UMAP...")
+    embedding_matrix = UMAP(n_components=2).fit_transform(embedding_matrix)
+    assert isinstance(embedding_matrix, np.ndarray)
+
+    # Make a scatter plot of the 2D embedding, where the country codes are colored
+    logger.info("Creating scatter plot...")
+    fig = px.scatter(
+        x=embedding_matrix[:, 0],
+        y=embedding_matrix[:, 1],
+        color=survey_df.country_code.tolist(),
+        title="UMAP projection of the EVS trend data",
+        labels=dict(x="UMAP 1", y="UMAP 2", color="Country Code"),
+        color_discrete_sequence=px.colors.qualitative.Plotly,
+        width=800,
+        height=600,
+    )
+    fig.update_traces(marker=dict(size=5))
+    fig.update_layout(
+        title_font=dict(size=20), legend_title_font=dict(size=16), font=dict(size=14)
+    )
+    fig.show()
--- src/scripts/build_dataset.py
+++ src/scripts/build_dataset.py
@@ -7,15 +7,10 @@ Usage:
 import logging
 
 import hydra
-import numpy as np
-import plotly.express as px
+from datasets import Dataset
 from omegaconf import DictConfig
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.experimental import enable_iterative_imputer  # noqa: F401
-from sklearn.impute import IterativeImputer
-from umap import UMAP
 
-from european_values.data_loading import load_evs_trend_data
+from european_values.data_loading import load_evs_trend_data, load_evs_wvs_data
 
 logger = logging.getLogger("build_dataset")
 
@@ -28,47 +23,17 @@ def main(config: DictConfig) -> None:
         config:
             The Hydra config for your project.
     """
+    # Load and push the EVS trend data to the Hugging Face Hub
     evs_trend_df = load_evs_trend_data()
-
-    # TEMP: Focus on a single wave
-    evs_trend_df = evs_trend_df.query(
-        "year > 1985 and year < 1995 and country_code in ['DK', 'SE', 'NO', 'IS', 'US']"
+    Dataset.from_pandas(evs_trend_df, preserve_index=False).push_to_hub(
+        repo_id=config.repo_id, config_name="evs_trend_data_1981_2017", private=True
     )
-    logger.info(f"Shape of the sliced data: {evs_trend_df.shape}")
 
-    # Create a 2-dimensional embedding of the EVS trend data
-    logger.info("Imputing missing values...")
-    embedding_matrix = IterativeImputer(
-        estimator=RandomForestClassifier(n_estimators=100, n_jobs=-1),
-        skip_complete=True,
-        n_nearest_features=10,
-        initial_strategy="most_frequent",
-        max_iter=20,
-        random_state=4242,
-    ).fit_transform(evs_trend_df.iloc[:, 3:])
-    logger.info(f"Shape of the imputed data: {embedding_matrix.shape}")
-
-    logger.info("Reducing to two dimensions with UMAP...")
-    embedding_matrix = UMAP(n_components=2).fit_transform(embedding_matrix)
-    assert isinstance(embedding_matrix, np.ndarray)
-
-    # Make a scatter plot of the 2D embedding, where the country codes are colored
-    logger.info("Creating scatter plot...")
-    fig = px.scatter(
-        x=embedding_matrix[:, 0],
-        y=embedding_matrix[:, 1],
-        color=evs_trend_df.country_code.tolist(),
-        title="UMAP projection of the EVS trend data",
-        labels=dict(x="UMAP 1", y="UMAP 2", color="Country Code"),
-        color_discrete_sequence=px.colors.qualitative.Plotly,
-        width=800,
-        height=600,
+    # Load and push the EVS/WVS data to the Hugging Face Hub
+    evs_wvs_df = load_evs_wvs_data()
+    Dataset.from_pandas(evs_wvs_df, preserve_index=False).push_to_hub(
+        repo_id=config.repo_id, config_name="evs_wvs_data_2017_2022", private=True
     )
-    fig.update_traces(marker=dict(size=5))
-    fig.update_layout(
-        title_font=dict(size=20), legend_title_font=dict(size=16), font=dict(size=14)
-    )
-    fig.show()
 
 
 if __name__ == "__main__":
