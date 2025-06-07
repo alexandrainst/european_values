9c2d315fabc194bdbd3cdbc74ae2bbf0308aa104
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-06-07 15:02:22 +0200

    feat: Allow specifying countries when plotting

--- src/scripts/create_plot.py
+++ src/scripts/create_plot.py
@@ -11,12 +11,13 @@ import pandas as pd
 from datasets import Dataset, load_dataset
 from omegaconf import DictConfig
 
+from european_values.data_processing import process_data
 from european_values.plotting import create_scatter
 
 logger = logging.getLogger("create_plot")
 
 
-@hydra.main(config_path="../../config", config_name="plotting", version_base=None)
+@hydra.main(config_path="../../config", config_name="config", version_base=None)
 def main(config: DictConfig) -> None:
     """Main function.
 
@@ -24,12 +25,13 @@ def main(config: DictConfig) -> None:
         config:
             The Hydra config for your project.
     """
+    dataset_id_suffix = "_processed" if config.countries == "all" else ""
     match config.data:
         case "evs_trend":
             logger.info("Using EVS trend data.")
             dataset = load_dataset(
                 path=config.repo_id,
-                name="evs_trend_data_1981_2017_processed",
+                name="evs_trend_data_1981_2017" + dataset_id_suffix,
                 split="train",
             )
             assert isinstance(dataset, Dataset)
@@ -39,7 +41,7 @@ def main(config: DictConfig) -> None:
             logger.info("Using EVS/WVS data.")
             dataset = load_dataset(
                 path=config.repo_id,
-                name="evs_wvs_data_2017_2022_processed",
+                name="evs_wvs_data_2017_2022" + dataset_id_suffix,
                 split="train",
             )
             assert isinstance(dataset, Dataset)
@@ -56,6 +58,12 @@ def main(config: DictConfig) -> None:
                 "'evs_trend' or 'evs_wvs'."
             )
     assert isinstance(df, pd.DataFrame)
+
+    if config.countries != "all":
+        logger.info(f"Filtering data for countries: {config.countries}")
+        df = df.query("country_code in @config.countries").reset_index(drop=True)
+        df = process_data(df=df, imputation_neighbours=config.imputation_neighbours)
+        logger.info(f"Shape of the data after filtering: {df.shape}")
 
     create_scatter(survey_df=df, dataset_name=dataset_name, config=config)
 
