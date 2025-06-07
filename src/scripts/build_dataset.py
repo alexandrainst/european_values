92ca75ebbb7220b43d6d1c5557b94e58c8034509
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-06-07 15:21:32 +0200

    fix: Do not build processed data in build_dataset script

--- src/scripts/build_dataset.py
+++ src/scripts/build_dataset.py
@@ -11,7 +11,6 @@ from datasets import Dataset
 from omegaconf import DictConfig
 
 from european_values.data_loading import load_evs_trend_data, load_evs_wvs_data
-from european_values.data_processing import process_data
 
 logger = logging.getLogger("build_dataset")
 
@@ -30,30 +29,10 @@ def main(config: DictConfig) -> None:
         repo_id=config.repo_id, config_name="evs_wvs_data_2017_2022", private=True
     )
 
-    # Process and push the EVS/WVS data to the Hugging Face Hub
-    processed_evs_wvs_df = process_data(
-        df=evs_wvs_df, imputation_neighbours=config.imputation_neighbours
-    )
-    Dataset.from_pandas(processed_evs_wvs_df, preserve_index=False).push_to_hub(
-        repo_id=config.repo_id,
-        config_name="evs_wvs_data_2017_2022_processed",
-        private=True,
-    )
-
     # Load and push the EVS trend data to the Hugging Face Hub
     evs_trend_df = load_evs_trend_data()
     Dataset.from_pandas(evs_trend_df, preserve_index=False).push_to_hub(
         repo_id=config.repo_id, config_name="evs_trend_data_1981_2017", private=True
-    )
-
-    # Process and push the EVS trend data to the Hugging Face Hub
-    processed_evs_trend_df = process_data(
-        df=evs_trend_df, imputation_neighbours=config.imputation_neighbours
-    )
-    Dataset.from_pandas(processed_evs_trend_df, preserve_index=False).push_to_hub(
-        repo_id=config.repo_id,
-        config_name="evs_trend_data_1981_2017_processed",
-        private=True,
     )
 
 
