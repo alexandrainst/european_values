95cfffca70e4094a6f75cd3ac56a32afeec9d45d
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-06-10 11:55:17 +0000

    fix: Do not mention ellipses in the title if they are not used

--- src/european_values/plotting.py
+++ src/european_values/plotting.py
@@ -107,9 +107,12 @@ def create_scatter(survey_df: pd.DataFrame, config: DictConfig) -> None:
     ax.scatter(
         x=country_embedding_matrix[:, 0], y=country_embedding_matrix[:, 1], alpha=0.0
     )
-    ax.set_title(
-        f"UMAP projection with ellipse radii = {config.ellipse_std}σ", fontsize=20
-    )
+    if config.ellipses:
+        ax.set_title(
+            f"UMAP projection with ellipse radii = {config.ellipse_std}σ", fontsize=20
+        )
+    else:
+        ax.set_title("UMAP projection", fontsize=20)
     plt.show()
 
 
